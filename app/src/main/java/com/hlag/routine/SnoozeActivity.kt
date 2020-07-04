d671789a84f5394251d95a9d7e88e8559aae40f9
Henrik Lagrosen <henrik.lagrosen@gmail.com>
2020-07-04 15:25:35 +0200

    snooze btn on notification

--- app/src/main/java/com/hlag/routine/SnoozeActivity.kt
+++ app/src/main/java/com/hlag/routine/SnoozeActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
 import android.view.WindowManager
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_snooze.*
 
 
 class SnoozeActivity : AppCompatActivity() {
+    var snoozeSet = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_snooze)
@@ -21,16 +22,26 @@ class SnoozeActivity : AppCompatActivity() {
                 if (event.action == KeyEvent.ACTION_DOWN &&
                     keyCode == KeyEvent.KEYCODE_ENTER
                 ) {
-                    Toast.makeText(this@SnoozeActivity, delay_min.text.toString(), Toast.LENGTH_SHORT)
-                        .show()
-                    val app = (application as MyApp)
-                    app.startTimer(app.activeStep!!, delay_min.text.toString().toInt()*60)
+                    setSnooze(delay_min.text.toString().toInt()*60)
                     finish()
                     return true
                 }
                 return false
             }
         })
+    }
 
+    fun setSnooze(sec: Int){
+        val app = (application as MyApp)
+        app.startTimer(app.activeStep!!, sec)
+        snoozeSet = true
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if(!snoozeSet){
+            setSnooze(60)
+            finish()
+        }
     }
 }
