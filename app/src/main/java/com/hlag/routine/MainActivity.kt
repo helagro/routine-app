fcd93f9abeca934b6baa1e457c599daa0a37b658
I <henrikl@tuta.io>
2021-07-22 09:08:48 +0200

    makes permission process less confusing for user

--- app/src/main/java/com/hlag/routine/MainActivity.kt
+++ app/src/main/java/com/hlag/routine/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.FragmentManager
 
@@ -28,73 +29,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //Permissions
-        if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        permissionsHandler()
+    }
+
+    private fun permissionsHandler(){
+        if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            openNextActivity()
+        else
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
-            return
-        }
-
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
-        routines_gridview.adapter = RoutineAdapter(this, routines)
-        routines_gridview.setOnItemClickListener {parent, view, position, id ->
-            startActivity(Intent(this, StepsActivity::class.java).putExtra("routine_name", routines.get(position).name))
-        }
-
-        fab.setOnClickListener { view ->
-            val dialog = TextDialog(this)
-            dialog.setOnDismissListener {
-                val text = dialog.editText.text.toString()
-                val routine = Routine(text, ArrayList<Step>())
-
-                FileManager.writeRoutine(this, routine)
-
-                routines.add(routine)
-                (routines_gridview.adapter as RoutineAdapter).notifyDataSetChanged()
-            }
-
-        }
-        loadFromStorage()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    private fun openNextActivity(){
+        startActivity(Intent(this, RoutinePicker::class.java))
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-        when (item.itemId) {
-            R.id.action_settings -> {
-                val builder = AlertDialog.Builder(this.applicationContext)
-                builder.setTitle("Test dialog")
-                builder.setMessage("Content")
-                val alert = builder.create()
-                alert.getWindow()!!.setType(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY)
-                alert.show()
-                return true
-            }
-            R.id.action_setFolder -> {
-                DirDialog().show(supportFragmentManager, "tag")
-            }
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun loadFromStorage(){
-        val dir = File(FileManager.dir)
-
-        if(!dir.isDirectory){
-            Log.d("Is file", dir.toString())
-            return
-        }
-
-        for(file in dir.listFiles()){
-            routines.add(Routine(file.nameWithoutExtension, ArrayList<Step>()))
-        }
-        (routines_gridview.adapter as RoutineAdapter).notifyDataSetChanged()
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults[0] == 0)
+            openNextActivity()
+        else
+            Toast.makeText(this, "The permission is needed to store routines, please restart the app", Toast.LENGTH_LONG).show()
     }
 }
---
+++ app/src/main/java/com/hlag/routine/RoutinePicker.kt
@@ -0,0 +1,87 @@
+package com.hlag.routine
+
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
+class RoutinePicker : AppCompatActivity() {
+    var routines: ArrayList<Routine> = arrayListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        routines_gridview.adapter = RoutineAdapter(this, routines)
+        routines_gridview.setOnItemClickListener {parent, view, position, id ->
+            startActivity(Intent(this, StepsActivity::class.java).putExtra("routine_name", routines.get(position).name))
+        }
+
+        fab.setOnClickListener { view ->
+            val dialog = TextDialog(this)
+            dialog.setOnDismissListener {
+                val text = dialog.editText.text.toString()
+                val routine = Routine(text, ArrayList<Step>())
+
+                FileManager.writeRoutine(this, routine)
+
+                routines.add(routine)
+                (routines_gridview.adapter as RoutineAdapter).notifyDataSetChanged()
+            }
+
+        }
+        loadFromStorage()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.action_settings -> {
+                val builder = AlertDialog.Builder(this.applicationContext)
+                builder.setTitle("Test dialog")
+                builder.setMessage("Content")
+                val alert = builder.create()
+                alert.getWindow()!!.setType(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY)
+                alert.show()
+                return true
+            }
+            R.id.action_setFolder -> {
+                DirDialog().show(supportFragmentManager, "tag")
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun loadFromStorage(){
+        val dir = File(FileManager.dir)
+
+        if(!dir.isDirectory){
+            Log.d("Is file", dir.toString())
+            return
+        }
+
+        for(file in dir.listFiles()){
+            routines.add(Routine(file.nameWithoutExtension, ArrayList<Step>()))
+        }
+        (routines_gridview.adapter as RoutineAdapter).notifyDataSetChanged()
+    }
+}
